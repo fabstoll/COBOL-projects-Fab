a833627318daddc1c690a9669db6cc4f1553e966
fabrice Stoll <fabstoll@gmail.com>
2025-09-21 13:03:01 +0200

    Add files via upload

---
+++ VerifPalindrome.cbl
@@ -0,0 +1,71 @@
+       PROGRAM-ID. PALINDROME.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+       01 WS-CHAINE PIC X(15).
+       01 WS-POINTEUR PIC 99 value 1.
+       01 WS-CHAINE-DEPLACEE PIC X(15).
+       01 WS-CHAINE-SANITIZED PIC X(15).
+       01 WS-CHAINE-INVERSEE PIC X(15).
+
+       PROCEDURE DIVISION.
+
+           PERFORM SAISIE-MOT
+           PERFORM GESTION-CASSE
+           PERFORM INVERSION
+           PERFORM DEPLACEMENT-MOT
+           PERFORM AFFICHAGE-VERIF
+           .
+
+           STOP RUN.
+
+      *--------------------------------------------- 
+       SAISIE-MOT.
+      *---------------------------------------------
+
+       DISPLAY "Saisir un mot (max 15 caractères) : ".
+       ACCEPT WS-CHAINE.
+
+      *--------------------------------------------- 
+       GESTION-CASSE.
+      *--- mettre toutes les letttres en minuscule
+
+       MOVE FUNCTION LOWER-CASE(WS-CHAINE) TO WS-CHAINE-SANITIZED.
+
+      *--------------------------------------------- 
+       INVERSION.
+      *--- Inversion du mot saisi 
+
+       MOVE FUNCTION REVERSE(WS-CHAINE-SANITIZED) TO WS-CHAINE-INVERSEE.
+
+      *--------------------------------------------- 
+       DEPLACEMENT-MOT.
+      *--- Comme l'inversion a également pris en compte les espaces,
+      *    il faut déplacer le mot en début d'
+   
+       STRING FUNCTION TRIM(WS-CHAINE-INVERSEE LEADING)
+           DELIMITED BY SIZE
+           INTO WS-CHAINE-DEPLACEE
+           WITH POINTER WS-POINTEUR
+           .
+
+      *--------------------------------------------- 
+       AFFICHAGE-VERIF.
+      *----Affichage des variables + vérification si palindrome
+
+           DISPLAY "    Chaîne d'origine : " WS-CHAINE.
+           DISPLAY "Chaîne en minuscules : " WS-CHAINE-SANITIZED.
+           DISPLAY "     Chaîne inversée : " WS-CHAINE-INVERSEE.
+           DISPLAY "          Mot décalé : " WS-CHAINE-DEPLACEE.
+           
+
+           IF WS-CHAINE-SANITIZED = WS-CHAINE-DEPLACEE
+               DISPLAY "La chaîne est un palindrome"
+           ELSE
+               DISPLAY "nix palindrome"
+           END-IF
+           .
+
